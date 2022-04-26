505e2e8e8ab42169ffd0dae68c1637f42125df54
Yu Kuan Lin <>
2022-04-26 13:26:47 +0800

    v2 test

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -14,7 +14,10 @@ public class Hello extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println("Hello from Servlet");
+		out.println("Hello from Servlet");
 	}
+	
+	
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
