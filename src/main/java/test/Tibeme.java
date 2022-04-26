f90a0053a61d4db871fc81b50aa5b0ba7e7d269d
Yu Kuan Lin <>
2022-04-26 16:05:58 +0800

    tibame > <

---
+++ src/main/java/test/Tibeme.java
@@ -0,0 +1,34 @@
+package test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Tibeme
+ */
+@WebServlet("/Tibeme")
+public class Tibeme extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+ 
+    public Tibeme() {
+        super();
+
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doPost(request, response);;
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().print("哈囉");
+	}
+
+}
