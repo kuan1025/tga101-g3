41c88411bcb8436035731f2357292bcbbcf715d6
Yu Kuan Lin <>
2022-04-26 16:21:24 +0800

    hello

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -11,6 +11,15 @@ import javax.servlet.http.HttpServletResponse;
 public class Hello extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	
+	
+	
+	/*
+	 * @Author Kuan
+	 * 
+	 */
+	
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println("Hello from Servlet");
